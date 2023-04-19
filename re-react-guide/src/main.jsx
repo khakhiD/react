adbccd1b2edfe53eea621de4f40a98653b72ab67
khakhiD <kakkiid@gmail.com>
2023-04-19 23:16:40 +0900

    re-react-guide - 리액트 전체 정리 - (11/37)

--- re-react-guide/src/main.jsx
+++ re-react-guide/src/main.jsx
@@ -4,6 +4,12 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  /*
+    React.StrictMode
+    작성한 코드가 최적인지 아닌지 확인하여 알려주는 기능,
+    추후 리액트가 업데이트되었을 때 작성 코드가 호환되지 않는 상황에 경고를 주는 기능
+  */ 
+
   <React.StrictMode>
     <App />
   </React.StrictMode>,
