9e10509eba7eb8c2b8fe9f26d65df2b648359c18
khakhiD <kakkiid@gmail.com>
2023-04-20 20:04:03 +0900

    state, state lifting (15/37)

--- re-react-guide/src/components/PostList.jsx
+++ re-react-guide/src/components/PostList.jsx
@@ -1,13 +1,28 @@
 import Post from "./Post";
 import classes from "./PostList.module.css";
+import { useState } from "react";
 import NewPost from "./NewPost";
 
 function PostList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost/>
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post author="Maximilian" body="React.js is awesome!" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manuel" body="Check out the full course!" />
       </ul>
     </>
