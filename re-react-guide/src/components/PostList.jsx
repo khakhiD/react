fc3bf1b37008ed4140e5128a6faa9ba79c0f3631
khakhiD <kakkiid@gmail.com>
2023-04-20 23:11:11 +0900

    re-react (23/37) basic done.

--- re-react-guide/src/components/PostList.jsx
+++ re-react-guide/src/components/PostList.jsx
@@ -1,30 +1,36 @@
 import Post from "./Post";
 import classes from "./PostList.module.css";
-import { useState } from "react";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
-function PostList() {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
+function PostList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
 
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
+  const addPostHandler = function (postData) {
+    // 새로 만든 포스트가 배열 첫 번째에 들어가고,
+    // 그 뒤로 원래 있던 포스트가 들어간다.
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  };
 
   return (
     <>
-      <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
-      />
-      <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
-        <Post author="Manuel" body="Check out the full course!" />
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
+        </Modal>
+      )}
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+        {posts.map((post) => <Post key={uuidv4()} author={post.author} body={post.body}/>)}
       </ul>
+      )}
+      {posts.length === 0 && <div style={{textAlign: 'center', color: 'lightgray'}}>
+          <h3>아직 작성된 포스트가 없습니다.</h3>
+          <p>포스트를 작성해보세요!</p>
+        </div>
+      }
     </>
   );
 }
