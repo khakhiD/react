67fcf9f52327b48963d915dda39305934caf428a
khakhiD <kakkiid@gmail.com>
2023-04-20 23:16:00 +0900

    -

--- re-react-guide/src/components/PostList.jsx
+++ re-react-guide/src/components/PostList.jsx
@@ -3,7 +3,7 @@ import classes from "./PostList.module.css";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 import { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 function PostList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
@@ -23,14 +23,17 @@ function PostList({ isPosting, onStopPosting }) {
       )}
       {posts.length > 0 && (
         <ul className={classes.posts}>
-        {posts.map((post) => <Post key={uuidv4()} author={post.author} body={post.body}/>)}
-      </ul>
+          {posts.map((post) => (
+            <Post key={uuidv4()} author={post.author} body={post.body} />
+          ))}
+        </ul>
       )}
-      {posts.length === 0 && <div style={{textAlign: 'center', color: 'lightgray'}}>
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "lightgray" }}>
           <h3>아직 작성된 포스트가 없습니다.</h3>
           <p>포스트를 작성해보세요!</p>
         </div>
-      }
+      )}
     </>
   );
 }
