be76ac43fe446385c67c27ce739d658b43524972
khakhiD <kakkiid@gmail.com>
2023-04-21 20:44:29 +0900

    feat: loading func

--- re-react-guide/src/components/PostList.jsx
+++ re-react-guide/src/components/PostList.jsx
@@ -7,12 +7,15 @@ import { v4 as uuidv4 } from "uuid";
 
 function PostList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function fetchPosts() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:8080/posts");
       const resData = await response.json();
       setPosts(resData.posts);
+      setIsFetching(false);
     }
 
     fetchPosts();
@@ -41,17 +44,22 @@ function PostList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={uuidv4()} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "lightgray" }}>
           <h3>아직 작성된 포스트가 없습니다.</h3>
           <p>포스트를 작성해보세요!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: 'center', color: 'white'}}>
+          <p>Loading...</p>
         </div>
       )}
     </>
