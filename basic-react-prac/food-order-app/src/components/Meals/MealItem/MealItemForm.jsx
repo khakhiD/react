988f78825231289f234aa7f3f73f549e2f687b44
khakhiD <kakkiid@gmail.com>
2023-05-13 20:10:36 +0900

    refactor: MealItemForm 고유 id로 변경

--- basic-react-prac/food-order-app/src/components/Meals/MealItem/MealItemForm.jsx
+++ basic-react-prac/food-order-app/src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import Input from '../../UI/Input'
-import classes from './MealItemForm.module.css'
+import React from "react";
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
 
-const MealItemFrom = () => {
+const MealItemFrom = (props) => {
   return (
     <form className={classes.form}>
-      <Input label="Amount" input={{
-        id: 'amount',
-        type: 'number',
-        min: '1',
-        max: '5',
-        step: '1',
-        defaultValue: '1'
-       }} />
+      <Input
+        label="Amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+ 추가</button>
     </form>
-  )
-}
+  );
+};
 
-export default MealItemFrom
+export default MealItemFrom;
